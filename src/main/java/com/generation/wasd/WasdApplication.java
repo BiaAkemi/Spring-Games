61e4603adbfb5de954b2f238532880802da01544
BiaAkemi <beancaakemi@gmail.com>
2023-12-14 11:22:26 -0300

    first commit

---
+++ src/main/java/com/generation/wasd/WasdApplication.java
@@ -0,0 +1,13 @@
+package com.generation.wasd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/wasd/WasdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.wasd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
