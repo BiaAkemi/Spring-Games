ae8f8c17df42e904b21315b472cfff35505c7206
BiaAkemi <beancaakemi@gmail.com>
2023-12-14 14:57:27 -0300

    Corrigindo erro bean type could not be found

--- src/main/java/com/generation/wasd/WasdApplication.java
+++ src/main/java/com/generation/wasd/WasdApplication.java
@@ -2,8 +2,9 @@ package com.generation.wasd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
-@SpringBootApplication
+@SpringBootApplication @EnableSpringDataWebSupport 
 public class WasdApplication {
 
 	public static void main(String[] args) {
